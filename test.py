04762837ea4dbe17a2e3c7708af36f40deacf333
egedolmaci <dolmaciege@gmail.com>
2022-08-14 14:48:47 +0300

    add test.py

--- test.py
+++ test.py
@@ -3,6 +3,7 @@ from helpful_fucntions import compare_overlap
 from nltk.tokenize import word_tokenize
 from nltk.corpus import stopwords
 import re
+from collections import Counter
 
 def preprocess(input_sentence):
     input_sentence = input_sentence.lower()
@@ -11,12 +12,21 @@ def preprocess(input_sentence):
     return(tokens)
 
 df = pd.read_csv('./Mental_Health_FAQ.csv')
-user_message = preprocess('Hello everyone my name is Ege')
+
 
 df['Clean'] = df['Questions'].apply(word_tokenize)
-similarity_list = list()
-for i in df['Clean']:
-    similarity_list.append(compare_overlap(user_message, i))
 
-print(similarity_list)
+user_message = preprocess('What is mental health?')
+
+
+def find_intent_match(user_message):
+      bow_user_message = Counter(user_message)
+      similarity_list = list()
+      for i in df['Clean']:
+        similarity_list.append(compare_overlap(bow_user_message, i))
+      response_index = similarity_list.index(max(similarity_list))
+      print(len(similarity_list))
+      return response_index
+
+print(find_intent_match(user_message))
 
